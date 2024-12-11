450477e7aff6c9108742041bb9df871ca08e4036
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2024-12-11 14:10:07 +0700

    feat: update import path

--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
-from component.wheel import *
+from .component.wheel import *
+
 
 class RobotControl:
     """
@@ -11,13 +12,18 @@ class RobotControl:
         "pwm2" : 18,
         "enb1" : 14,
         "enb2" : 15,
-    } 
+    }
 
 
     """
+
     def __init__(self, left, right):
-        self.left_wheel = MotorDriver(left["pwm1"], left["pwm2"], left["enb1"], left["enb2"])
-        self.right_wheel = MotorDriver(right["pwm1"], right["pwm2"], right["enb1"], right["enb2"])
+        self.left_wheel = MotorDriver(
+            left["pwm1"], left["pwm2"], left["enb1"], left["enb2"]
+        )
+        self.right_wheel = MotorDriver(
+            right["pwm1"], right["pwm2"], right["enb1"], right["enb2"]
+        )
 
     def robot_forward(self, speed=25):
         self.left_wheel.move_reverse(speed=speed)
@@ -29,11 +35,11 @@ class RobotControl:
 
     def robot_turn_right(self, speed=25):
         self.left_wheel.move_reverse(speed=speed)
-        self.right_wheel.move_forward(speed=speed/2)
+        self.right_wheel.move_forward(speed=speed / 2)
 
     def robot_turn_left(self, speed=25):
         self.left_wheel.move_forward(speed=speed)
-        self.right_wheel.move_reverse(speed=speed/2)
+        self.right_wheel.move_reverse(speed=speed / 2)
 
     def robot_pivot_right(self, speed=25):
         self.left_wheel.move_reverse(speed=speed)
