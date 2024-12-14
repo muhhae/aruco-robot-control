b5ba712d4826ac66bece463f0b43331029cbf465
Ariqusu77 <ariqtazakka@gmail.com>
2024-12-15 01:09:37 +0700

    correction path

--- robot.py
+++ robot.py
@@ -14,8 +14,9 @@ class RobotControl:
         "enb2" : 15,
     }
 
-
     """
+
+    left_right_ratio = 23/25
 
     def __init__(self, left, right):
         self.left_wheel = MotorDriver(
@@ -24,44 +25,64 @@ class RobotControl:
         self.right_wheel = MotorDriver(
             right["pwm1"], right["pwm2"], right["enb1"], right["enb2"]
         )
+        self.left_speed = 0
+        self.right_speed = 0
 
-    def robot_forward(self, speed=25):
-        self.left_wheel.move_reverse(speed=speed)
-        self.right_wheel.move_forward(speed=speed)
+    def robot_forward(self, speed=20):
+        self.right_speed = speed * self.left_right_ratio
+        self.left_speed = speed
+        self.left_wheel.move_reverse(speed=self.left_speed)
+        self.right_wheel.move_forward(speed=self.right_speed)
 
     def robot_backward(self, speed=25):
-        self.left_wheel.move_forward(speed=speed)
-        self.right_wheel.move_reverse(speed=speed)
+        self.right_speed = speed * self.left_right_ratio
+        self.left_speed = speed
+        self.left_wheel.move_forward(speed=self.left_speed)
+        self.right_wheel.move_reverse(speed=self.right_speed)
 
     def robot_turn_right(self, speed=25):
-        self.left_wheel.move_reverse(speed=speed)
-        self.right_wheel.move_forward(speed=speed / 2)
+        self.right_speed = (speed / 2) * self.left_right_ratio
+        self.left_speed = speed
+        self.left_wheel.move_reverse(speed=self.left_speed)
+        self.right_wheel.move_forward(speed=self.right_speed)
 
     def robot_turn_left(self, speed=25):
-        self.left_wheel.move_forward(speed=speed)
-        self.right_wheel.move_reverse(speed=speed / 2)
+        self.right_speed = speed
+        self.left_speed = (speed / 2) * self.left_right_ratio
+        self.left_wheel.move_forward(speed=self.left_speed)
+        self.right_wheel.move_reverse(speed=self.right_speed)
 
     def robot_pivot_right(self, speed=25):
-        self.left_wheel.move_reverse(speed=speed)
-        self.right_wheel.move_reverse(speed=speed)
+        self.right_speed = speed * self.left_right_ratio
+        self.left_speed = speed
+        self.left_wheel.move_reverse(speed=self.left_speed)
+        self.right_wheel.move_reverse(speed=self.right_speed)
 
     def robot_pivot_left(self, speed=25):
-        self.left_wheel.move_forward(speed=speed)
-        self.right_wheel.move_forward(speed=speed)
+        self.right_speed = speed * self.left_right_ratio
+        self.left_speed = speed
+        self.left_wheel.move_forward(speed=self.left_speed)
+        self.right_wheel.move_forward(speed=self.right_speed)
 
     def robot_left_forward(self, speed=25):
-        self.left_wheel.move_forward(speed=speed)
+        self.left_speed = speed
+        self.left_wheel.move_forward(speed=self.left_speed)
 
     def robot_left_backward(self, speed=25):
-        self.left_wheel.move_backward(speed=speed)
+        self.left_speed = speed
+        self.left_wheel.move_backward(speed=self.left_speed)
 
     def robot_right_forward(self, speed=25):
-        self.right_wheel.move_forward(speed=speed)
+        self.right_speed = speed
+        self.right_wheel.move_forward(speed=self.right_speed)
 
     def robot_right_backward(self, speed=25):
-        self.right_wheel.move_backward(speed=speed)
+        self.right_speed = speed
+        self.right_wheel.move_backward(speed=self.right_speed)
 
     def robot_stop(self):
+        self.left_speed = 0
+        self.right_speed = 0
         self.left_wheel.stop_moving()
         self.right_wheel.stop_moving()
 
